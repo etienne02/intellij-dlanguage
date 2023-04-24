9204b827d02730ba7907446488d3358660f9091f
Etienne Brateau <etienne.brateau@gmail.com>
2023-04-24 13:40:39 +0200

    Rework module import builder to use the open project processor
    This will allow to have consistent import and open project

--- dub/src/main/kotlin/io/github/intellij/dub/project/DubProjectImportBuilder.kt
+++ dub/src/main/kotlin/io/github/intellij/dub/project/DubProjectImportBuilder.kt
@@ -1,37 +1,32 @@
 package io.github.intellij.dub.project
 
-import com.intellij.notification.NotificationGroupManager
-import com.intellij.notification.NotificationType
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.module.ModifiableModuleModel
 import com.intellij.openapi.module.Module
-import com.intellij.openapi.module.ModuleManager
-import com.intellij.openapi.module.ModuleWithNameAlreadyExists
-import com.intellij.openapi.options.ConfigurationException
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.roots.ui.configuration.ModulesProvider
-import com.intellij.openapi.util.InvalidDataException
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.packaging.artifacts.ModifiableArtifactModel
+import com.intellij.platform.backend.observation.launchTracked
+import com.intellij.projectImport.DeprecatedProjectBuilderForImport
 import com.intellij.projectImport.ProjectImportBuilder
+import com.intellij.projectImport.ProjectOpenProcessor
 import io.github.intellij.dlanguage.DLanguage
-import io.github.intellij.dlanguage.DlangSdkType
-import io.github.intellij.dlanguage.utils.DToolsNotificationAction
-import io.github.intellij.dub.Dub
-import io.github.intellij.dub.DubBundle
-import io.github.intellij.dub.module.DlangDubModuleBuilder
-import io.github.intellij.dub.project.DubConfigFileListener.Companion.getDubFileFromModule
-import io.github.intellij.dub.service.DubBinaryPathProvider
-import org.jdom.JDOMException
-import java.io.IOException
-import java.util.function.Consumer
+import io.github.intellij.dub.DubCoroutineScope.dubCoroutineScope
 import javax.swing.Icon
 
-class DubProjectImportBuilder : ProjectImportBuilder<DubPackage>() {
+
+/**
+ * Do not use this project import builder directly.
+ *
+ * Internal stable Api
+ * Use [com.intellij.ide.actions.ImportModuleAction.createFromWizard] to import (attach) a new project.
+ * Use [com.intellij.ide.impl.ProjectUtil.openOrImport] to open (import) a new project.
+ *
+ * Internal experimental Api
+ * Use [io.github.intellij.dub.project.openDubProject] to open (import) a new dub project.
+ * Use [io.github.intellij.dub.project.linkAndRefreshDubProject] to attach a dub project to an opened idea project.
+ */
+class DubProjectImportBuilder : ProjectImportBuilder<DubPackage>(), DeprecatedProjectBuilderForImport {
     private var parameters: Parameters? = null
-    fun setRootDirectory(path: String?) {}
     fun getParameters(): Parameters {
         if (parameters == null) {
             parameters = Parameters()
@@ -40,7 +35,7 @@ class DubProjectImportBuilder : ProjectImportBuilder<DubPackage>() {
     }
 
     override fun getName(): String {
-        return CLionDubProjectOpenProcessor.NAME
+        return DubProjectOpenProcessor.NAME
     }
 
     override fun getIcon(): Icon {
@@ -68,97 +63,16 @@ class DubProjectImportBuilder : ProjectImportBuilder<DubPackage>() {
         modulesProvider: ModulesProvider?,
         modifiableArtifactModel: ModifiableArtifactModel?
     ): List<Module> {
-        val model = modifiableModuleModel ?: ModuleManager.getInstance(project).getModifiableModel()
-        val modules: MutableList<Module> = ArrayList()
-        ApplicationManager.getApplication().runWriteAction {
-            commitSdk(project)
-            modules.addAll(buildModules(project, model))
+        project.dubCoroutineScope.launchTracked {
+            linkAndSyncDubProject(project, fileToImport)
         }
-        return modules
-    }
-
-    private fun findDubConfigFile(module: Module): VirtualFile? {
-        val file = getDubFileFromModule(module)
-        if (file != null) {
-            return file
-        }
-        NotificationGroupManager.getInstance()
-            .getNotificationGroup(Dub.NOTIFICATION_GROUP_ID)
-            .createNotification(
-                DubBundle.message("dub.import.notification.title"),
-                "Dub project does not seem to contain dub.json or dub.sdl.",
-                NotificationType.WARNING
-            )
-            .notify(module.project)
-        return null
-    }
-
-    private fun buildModules(
-        project: Project,
-        moduleModel: ModifiableModuleModel
-    ): List<Module> {
-        val moduleList: MutableList<Module> = ArrayList()
-        if (!DubBinaryPathProvider.isDubAvailable()) {
-            NotificationGroupManager.getInstance()
-                .getNotificationGroup(Dub.NOTIFICATION_GROUP_ID)
-                .createNotification(
-                    DubBundle.message("dub.import.notification.title"),
-                    "DUB executable path is empty",
-                    NotificationType.WARNING
-                )
-                .addAction(DToolsNotificationAction("Configure"))
-                .notify(project)
-            // prevent a crash, inform callers that we can't actually call dub
-            // not ideal, should really be handled elsewhere
-            throw ConfigurationException("Missing dub, cannot import")
-        }
-        val dubConfigurationParser = DubConfigurationParser(
-            project,
-            DubBinaryPathProvider.getDubPath()!!,
-            false,
-            this.fileToImport
-        )
-        val dubProject = dubConfigurationParser.dubProject
-        dubProject.ifPresent { (_, pkg): DubProject ->
-            val builder = DlangDubModuleBuilder()
-            builder.moduleFilePath = pkg.path + pkg.name + ".iml"
-            builder.contentEntryPath = pkg.path
-            builder.name = pkg.name
-            pkg.sourcesDirs.forEach(Consumer { dir: String -> builder.addSourcePath(pkg.path + dir) })
-            try {
-                val module = builder.createModule(moduleModel)
-                //                DubConfigFileListener
-//                    .addProcessDLibsListener(findDubConfigFile(module), project, module);
-                builder.commit(project)
-                moduleList.add(module)
-            } catch (e: InvalidDataException) {
-                LOG.error(e)
-            } catch (e: IOException) {
-                LOG.error(e)
-            } catch (e: JDOMException) {
-                LOG.error(e)
-            } catch (e: ModuleWithNameAlreadyExists) {
-                LOG.error(e)
-            } catch (e: ConfigurationException) {
-                LOG.error(e)
-            }
-        }
-        return moduleList
-    }
-
-    private fun commitSdk(project: Project) {
-        ProjectRootManager.getInstance(project).projectSdk = DlangSdkType.findOrCreateSdk()
+        return emptyList()
     }
 
     class Parameters {
         var packages: List<DubPackage>? = null
-        @JvmField
-        var dubBinary: String? = null
     }
 
-    companion object {
-        private val LOG = Logger.getInstance(
-            DubProjectImportBuilder::class.java
-        )
-    }
+    override fun getProjectOpenProcessor(): ProjectOpenProcessor =
+        ProjectOpenProcessor.EXTENSION_POINT_NAME.findExtensionOrFail(DubProjectOpenProcessor::class.java)
 }
