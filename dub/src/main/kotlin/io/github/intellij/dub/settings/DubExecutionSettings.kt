7a5d880294c4571e7968c911b82654a141838af9
Etienne Brateau <etienne.brateau@gmail.com>
2023-05-09 11:20:53 +0200

    Start to implement the project resolver

--- dub/src/main/kotlin/io/github/intellij/dub/settings/DubExecutionSettings.kt
+++ dub/src/main/kotlin/io/github/intellij/dub/settings/DubExecutionSettings.kt
@@ -3,4 +3,6 @@ package io.github.intellij.dub.settings
 import com.intellij.openapi.externalSystem.model.settings.ExternalSystemExecutionSettings
 
 class DubExecutionSettings : ExternalSystemExecutionSettings() {
+
+    var ideProjectPath: String? = null
 }
