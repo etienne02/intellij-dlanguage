435ad7e111abb485e111219cd6a389d653379819
Etienne Brateau <etienne.brateau@gmail.com>
2023-05-09 11:20:53 +0200

    Start to implement the project resolver

--- dub/src/main/kotlin/io/github/intellij/dub/project/DubProjectResolver.kt
+++ dub/src/main/kotlin/io/github/intellij/dub/project/DubProjectResolver.kt
@@ -2,16 +2,18 @@ package io.github.intellij.dub.project
 
 import com.intellij.openapi.externalSystem.importing.ProjectResolverPolicy
 import com.intellij.openapi.externalSystem.model.DataNode
+import com.intellij.openapi.externalSystem.model.ProjectKeys
+import com.intellij.openapi.externalSystem.model.project.ContentRootData
+import com.intellij.openapi.externalSystem.model.project.ModuleData
 import com.intellij.openapi.externalSystem.model.project.ProjectData
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListener
 import com.intellij.openapi.externalSystem.service.project.ExternalSystemProjectResolver
-import com.intellij.util.containers.MultiMap
+import io.github.intellij.dub.Dub
 import io.github.intellij.dub.settings.DubExecutionSettings
+import java.io.File
 
 class DubProjectResolver : ExternalSystemProjectResolver<DubExecutionSettings> {
-
-    //private val cancellationMap: MultiMap<ExternalSystemTaskId, CancellationTokenSource> = MultiMap.createConcurrent()
 
     override fun resolveProjectInfo(
         id: ExternalSystemTaskId,
@@ -20,11 +22,36 @@ class DubProjectResolver : ExternalSystemProjectResolver<DubExecutionSettings> {
         settings: DubExecutionSettings?,
         resolverPolicy: ProjectResolverPolicy?,
         listener: ExternalSystemTaskNotificationListener
-    ): DataNode<ProjectData>? {
-        return null
+    ): DataNode<ProjectData> {
+        if (isPreviewMode) {
+            val projectName = File(projectPath).name
+            val projectData = ProjectData(Dub.SYSTEM_ID, projectName, projectPath, projectPath)
+            val projectDataNode = DataNode(ProjectKeys.PROJECT, projectData, null)
+
+            val ideProjectPath = settings?.ideProjectPath
+            val mainModuleFileDirectoryPath = ideProjectPath?:projectPath
+            val moduleData = ModuleData(projectName, Dub.SYSTEM_ID, "D",
+                projectName, mainModuleFileDirectoryPath, projectPath)
+            projectDataNode
+                .createChild(ProjectKeys.MODULE, moduleData)
+                .createChild(ProjectKeys.CONTENT_ROOT, ContentRootData(Dub.SYSTEM_ID, projectPath))
+            return projectDataNode
+        }
+        val projectName = File(projectPath).name
+        val projectData = ProjectData(Dub.SYSTEM_ID, projectName, projectPath, projectPath)
+        val projectDataNode = DataNode(ProjectKeys.PROJECT, projectData, null)
+
+        val ideProjectPath = settings?.ideProjectPath
+        val mainModuleFileDirectoryPath = ideProjectPath?:projectPath
+        // TODO use dub to create all path and others
+        val moduleData = ModuleData(projectName, Dub.SYSTEM_ID, "D",
+            projectName, mainModuleFileDirectoryPath, projectPath)
+        projectDataNode
+            .createChild(ProjectKeys.MODULE, moduleData)
+            .createChild(ProjectKeys.CONTENT_ROOT, ContentRootData(Dub.SYSTEM_ID, projectPath))
+        return projectDataNode
     }
     override fun cancelTask(taskId: ExternalSystemTaskId, listener: ExternalSystemTaskNotificationListener): Boolean {
-
         return true
     }
 }
