11a8e54e1275a97a50c8e30490de70be863cfbfe
Etienne Brateau <etienne.brateau@gmail.com>
2024-09-02 16:47:21 +0200

    Improve handling of incomplete/invalid cases
    With this patch, the errors will be more useful for user, be more
    precise.

--- build.gradle.kts
+++ build.gradle.kts
@@ -97,6 +97,7 @@ dependencies {
     implementation (project(":sdlang"))
     implementation (project(":dlang:psi-impl"))
     testImplementation (project(":dlang:plugin-impl"))
+    testImplementation("org.opentest4j:opentest4j:1.3.0")
 
     implementation (libs.gson) // used by dub parser
 
